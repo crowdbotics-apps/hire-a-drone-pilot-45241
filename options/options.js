e883a77e0acf5609f03ca0261f6365cb2af0092d
Crowdbotics <team@crowdbotics.com>
2024-01-02 07:24:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hire-a-drone-pilot-45241",
+  url: "https://hire-a-drone-pilot-45241.botics.co",
+  api: "https://hire-a-drone-pilot-45241.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
